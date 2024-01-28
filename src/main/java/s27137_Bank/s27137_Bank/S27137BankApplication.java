93e3c8aea7c91fe7975f25dfb850e5a6d9357814
s27137-pj <s27137@pjwstk.edu.pl>
2024-01-28 15:07:20 +0100

    initial commit

---
+++ src/main/java/s27137_Bank/s27137_Bank/S27137BankApplication.java
@@ -0,0 +1,13 @@
+package s27137_Bank.s27137_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27137BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S27137BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s27137_Bank/s27137_Bank/S27137BankApplicationTests.java
@@ -0,0 +1,13 @@
+package s27137_Bank.s27137_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S27137BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
